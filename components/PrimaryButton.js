70687f20de9d2f40a7bc5c4fe914552eda2cec49
sejungkwak <kwak.sejung@gmail.com>
2026-01-12 17:58:24 +0000

    feat: create custom buttons

---
+++ components/PrimaryButton.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+function PrimaryButton({ children }) {
+    return (
+        <View>
+            <Text>
+                { children }
+            </Text>
+        </View>
+    )
+}
+
+export default PrimaryButton;
