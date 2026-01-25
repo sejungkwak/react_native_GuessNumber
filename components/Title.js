ec6d5607bfdded2b8ab4d04899d6947530a14dfe
sejungkwak <kwak.sejung@gmail.com>
2026-01-25 21:40:39 +0000

    feat: create a Title component

---
+++ components/Title.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text } from 'react-native'
+
+function Title({ children }) {
+    return <Text style={styles.title}>{children}</Text>
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+    title: {
+        padding: 12,
+        borderWidth: 2,
+        borderColor: '#ddb52f',
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#ddb52f',
+        textAlign: 'center',
+    }
+})
+
